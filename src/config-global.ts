c2f9e8c03e018b4ef9dd3e6aec0ac24b40a38011
zoljargal <zoljargal.b@nomin.net>
2025-09-11 13:13:27 +0800

    Updates

--- src/config-global.ts
+++ src/config-global.ts
@@ -1,1 +1,2 @@
-export const MAIN_API = process.env.NEXT_PUBLIC_MAIN_API
+// export const MAIN_API = process.env.NEXT_PUBLIC_MAIN_API || "https://cab21.zakhzeel.mn/cab21"
+export const MAIN_API = "https://cab21.zakhzeel.mn/cab21";
