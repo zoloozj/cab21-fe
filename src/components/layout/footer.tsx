44d35db7bfe83a0ae6fa963b191f4c89d5b8cd71
zoljargal <zoljargal.b@nomin.net>
2025-09-17 13:40:31 +0800

    U

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,17 +1,25 @@
+import Iconify from "../ui/iconify";
+
 export default function Footer() {
   return (
-    <div className="w-full h-16 flex items-center pt-3 border-t justify-between px-4 md:px-16 lg:px-40 mb-8">
+    <div className="w-full h-max flex-col sm:flex-row sm:flex gap-3 items-center pt-3 border-t justify-between px-4 md:px-16 lg:px-40 mb-8">
       <div className="flex flex-col gap-2 text-sm">
         <p>
-          <span className="text-gray-600">Холбоо барих:</span>{" "}
-          infocab21.zakhzeel@gmail.com
+          <span className="text-gray-600">Холбоо барих: </span>{" "}
+          <span className="text-nowrap">85151195</span>
         </p>
         <p>
-          <span className="text-gray-600">Хаяг:</span> 1711 Keybridge way
-          Bloomington IL 61704
+          <span className="text-gray-600">И-Мэйл:</span>{" "}
+          <span className="text-nowrap">infocab21.zakhzeel@gmail.com</span>
+        </p>
+        <p>
+          <span className="text-gray-600">Хаяг:</span>
+          <span className="text-nowrap">
+            1711 Keybridge way Bloomington IL 61704
+          </span>
         </p>
       </div>
-      <p className="text-sm text-gray-600">@ copyright</p>
+      <p className="text-sm text-gray-600 w-max">@copyright</p>
     </div>
   );
 }
