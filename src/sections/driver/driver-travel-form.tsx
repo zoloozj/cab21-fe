2eca1377b2d107a4f47884a5369668fee78ad293
zoloo <zoljargal.b@nomin.net>
2025-10-21 08:58:34 +0800

    Update

--- src/sections/driver/driver-travel-form.tsx
+++ src/sections/driver/driver-travel-form.tsx
@@ -94,6 +94,7 @@ export default function DriverTravelForm({ editD }: Props) {
     mutationFn: async (body: any) => {
       if (editD) {
         const { data } = await axios.put("/api/req", body);
+        return data;
       } else {
         const { data } = await axios.post("/api/req", body);
         return data;
@@ -125,8 +126,7 @@ export default function DriverTravelForm({ editD }: Props) {
       serviceUrl: editD ? `api/rides/edit/${editD?.id}` : "api/rides/create",
       cabId,
     };
-    if (editD) mutation.mutate(finalvalue);
-    else mutation.mutate(finalvalue);
+    mutation.mutate(finalvalue);
   }
 
   const [step, setStep] = useState(1);
